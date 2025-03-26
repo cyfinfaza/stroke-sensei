83ec4364e46bd9f976a0b5a704fb24f98cbc2586
Pavan datta Reddy <pvn.datta@gmail.com>
2025-03-26 11:29:25 -0400

    some changes

--- src/ML/model_test.py
+++ src/ML/model_test.py
@@ -6,7 +6,7 @@ import numpy as np # type: ignore
 model = tf.keras.models.load_model('kanji_model.h5')
 
 #preprocess image
-img_path = '../ML/input_images/kanji_1_input.png'  
+img_path = '../ML/input_images/kanji_drawing_1.png'  
 img = image.load_img(img_path, target_size=(28, 28), color_mode='grayscale')
 img_array = image.img_to_array(img) / 255.0  
 img_array = img_array.reshape((1, 28, 28, 1))
